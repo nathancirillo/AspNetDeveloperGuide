94661b86b30a76fc5d0ac9d43c41098a9ee52b49
Nathan Cirillo <nathan_cirillo@hotmail.com>
2025-02-23 01:06:57 -0300

    Entendendo o async await (programação assincrona).

---
+++ ExampleAsyncAwait/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("0. Iniciou a execução do programa;");
+
+//todo: verifique o resultado das diferenças de chamada:
+
+//usando da forma abaixo a thread é liberada
+// await CallMethodAsync();
+
+// CallMethodAsync();
+
+//usando da forma abaixo a thread é bloqueada (perigoso)
+// CallMethodAsync().Wait();
+
+Console.WriteLine("3. Finalizou a execução do programa.");
+
+
+
+static async Task CallMethodAsync(){
+  Console.WriteLine("1. Iniciou a execução do método;");
+  await Task.Delay(4000);
+  Console.WriteLine("2. Finalizou a execução do método;");
+}
